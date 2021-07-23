8ba8efa9c6395ccad19d55ae1e16b4905624f5e3
Chauntel <chauntelk@gmail.com>
2021-07-23 03:16:03 +0000

    clean/reformat

--- back-end/dev-docker-entrypoint.sh
+++ back-end/dev-docker-entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-echo "Environment: $RAILS_ENV"
+rm -f /usr/src/app/pids/server.pid
+mkdir -p /usr/src/app/tmp/pids
+touch /usr/src/app/tmp/pids/server.pid
 
-# install missing gems
-bundle check || bundle install --jobs 20 --retry 5
 
-# Remove pre-existing puma/passenger server.pid
-rm -f $APP_PATH/tmp/pids/server.pid
+# Initialize and seed database
+bundle exec rake db:setup
 
-# run passed commands
-bundle exec ${@}
+# Execute defualt instruction
+exec bundle exec "$@"
